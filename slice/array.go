a77bea7a358948c731526b5665553b12f95b27b2
douglasmg7 <douglasmg7@gmail.com>
2019-02-20 11:20:18 -0300

    diference beteween array and slice

--- slice/array.go
+++ slice/array.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 )
 
 // Array.
 // Documentation said, use slice instead.
 // Len is part of type.
+// If no len []int, will be a slice.
 var a [5]int
 
 func main() {
-  a[1] = 2
-  fmt.Println(a)
-  fmt.Println(len(a))
+	a[1] = 2
+	fmt.Println(a)
+	fmt.Println(len(a))
 }
--- slice/slice.go
+++ slice/slice.go
@@ -3,9 +3,10 @@ package main
 import "fmt"
 
 func main() {
-  // Slice of int.
-  s1 := []int{1, 2, 3, 4, 5, 6, 7, 8} // Composite literal.
-  fmt.Printf("Capacity: %v\n", cap(s1))
-  fmt.Printf("Length: %v\n", len(s1))
-  fmt.Printf("s1: %v\n", s1)
+	// Slice of int.
+	// If have len [5]int, will be a array.
+	s1 := []int{1, 2, 3, 4, 5, 6, 7, 8} // Composite literal.
+	fmt.Printf("Capacity: %v\n", cap(s1))
+	fmt.Printf("Length: %v\n", len(s1))
+	fmt.Printf("s1: %v\n", s1)
 }
