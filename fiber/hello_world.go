173b94ed60f86ad5bbc299f716291c34307afc12
douglasmg7 <douglasmg7@gmail.com>
2020-09-26 06:10:07 -0300

    fiber v2

--- fiber/hello_world.go
+++ fiber/hello_world.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"github.com/gofiber/fiber"
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
-	app.Get("/", func(c *fiber.Ctx) {
-		c.Send("Hello world!")
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello world!")
 	})
-	app.Listen(8080)
+	app.Listen(":8080")
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.14
 
 require (
 	github.com/gin-gonic/gin v1.6.3
-	github.com/gofiber/fiber v1.14.5
+	github.com/gofiber/fiber v1.14.6
+	github.com/gofiber/fiber/v2 v2.0.2
 )
